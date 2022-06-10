2b8cefa01b3952323fd5348d4ca606283d5021d6
Centre Team <cwma.campbell@gmail.com>
2022-06-10 22:33:14 +0100

    1st commit

---
+++ Radio/mplayer_perl.pl
@@ -0,0 +1,41 @@
+# send the track contents of mplayer output to file
+# perl mplayer_perl.pl B | stdbuf -o L grep 'ICY' > tracks.txt
+
+
+#!/usr/bin/perl
+use strict;
+use warnings;
+ 
+my $chan = shift || help();
+
+if ($chan eq 'A') {
+  radio("http://live.argyllfm.com/stream6?ref=RF.m3u");
+  }
+  elsif ($chan eq 'B') {
+    radio("http://streams.deltaradio.de/delta-grunge/aac-64");
+  }
+  elsif ($chan =~ m/xxxx/) {
+    radio();;
+  }
+  elsif ($chan =~ m/xxxx/) {
+    radio();
+  }
+  elsif ($chan =~ m/xxxx/) {
+    radio();
+  }
+  else { help(); }
+
+sub radio {
+ my ($url) = @_;
+ system("mplayer $url");
+}
+ 
+sub help {
+print "Usage: mplayer_perl.pl <channel>" , "\n";
+print "Channels: " , "\n";
+print "A - Argyll.FM\n";
+print "B - Delta Grunge\n";
+print "xxxx - something else\n";
+print "xxxx - something else\n";
+print "xxxx - something else\n";
+}
