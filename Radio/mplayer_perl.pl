3e03fcb34d4fa5d6ab341fe1225c3c94d695d143
Centre Team <cwma.campbell@gmail.com>
2022-06-11 17:52:44 +0100

    made changes to file output added radio stations

--- Radio/mplayer_perl.pl
+++ Radio/mplayer_perl.pl
@@ -1,35 +1,69 @@
 # send the track contents of mplayer output to file
-# perl mplayer_perl.pl B | stdbuf -o L grep 'ICY' > tracks.txt
-# buffering off autoflush on
-# try IO::Filehandle module
+# perl mplayer_perl.pl A or B or C or D or E
+
+# adding stdbuf grep and file redirect to system call in subroutine
+# this sends STDOUT of these grepped lines to file
+# IO::Tee can send it to standard output and the file
+
+
+# use IO:Tee;
+# my $teelog = "tee.log"
+# open my $tee, ">", $teelog or die "open tee failed.\n";
+# $tee = new IO:Tee(\*STDOUT, $tee);
+# print $tee "First line in log\n";
+
+# system("date | tee -a ${teelog}");
+# system("jibberish 2>&1 | tee -a ${teelog}");
+
+# open $tee, ">>", $teelog or die "open tee failed.\n";  # append mode
+# $tee = new IO:Tee(\*STDOUT, $tee);
+
+# streams to add to hash and help menu
+# http://streams.deltaradio.de/100/aac-64
+# http://streams.deltaradio.de/101/aac-64
+# http://streams.deltaradio.de/delta-alternative/aac-64
+# http://streams.deltaradio.de/delta-poprock/aac-64
+# http://streams.deltaradio.de/delta-foehnfrisur/aac-64
+# http://streams.deltaradio.de/delta-live/aac-64
+# http://streams.deltaradio.de/delta-sommer/aac-64
+
+
 
 
 #!/usr/bin/perl
 use strict;
 use warnings;
- 
+
+my %hash = (
+            'A' => "http://live.argyllfm.com/stream6?ref=RF.m3u",
+            'B' => "http://streams.deltaradio.de/delta-grunge/aac-64",
+            'C' => "http://streams.deltaradio.de/delta-unplugged/aac-64",
+            'D' => "http://streams.deltaradio.de/delta-indie/aac-64"
+           );
+
+my $log_file = 'tracks.txt';
 my $chan = shift || help();
 
 if ($chan eq 'A') {
-  radio("http://live.argyllfm.com/stream6?ref=RF.m3u");
+  radio($hash{A});
   }
   elsif ($chan eq 'B') {
-    radio("http://streams.deltaradio.de/delta-grunge/aac-64");
+    radio($hash{B});
   }
-  elsif ($chan =~ m/xxxx/) {
-    radio();;
+  elsif ($chan eq 'C') {
+    radio($hash{C});;
+  }
+  elsif ($chan eq 'D') {
+    radio($hash{D});
   }
   elsif ($chan =~ m/xxxx/) {
     radio();
   }
-  elsif ($chan =~ m/xxxx/) {
-    radio();
-  }
-  else { help(); }
+  else { help(); };
 
 sub radio {
- my ($url) = @_;
- system("mplayer $url");
+ my $url = shift @_;
+ system("mplayer $url | stdbuf -o L grep 'ICY' >> 'tracks.txt'");
 }
  
 sub help {
@@ -37,7 +71,7 @@ print "Usage: mplayer_perl.pl <channel>" , "\n";
 print "Channels: " , "\n";
 print "A - Argyll.FM\n";
 print "B - Delta Grunge\n";
-print "xxxx - something else\n";
-print "xxxx - something else\n";
+print "C - Delta Unplugged\n";
+print "D - Delta Indie\n";
 print "xxxx - something else\n";
 }
